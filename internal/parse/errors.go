e78fa734296f3451f8398587109635f6625fb814
fatihege <ifatxh@gmail.com>
2026-08-28 16:23:07 +0300

    parse: add ErrNotString into errors and update some error messages

--- internal/parse/errors.go
+++ internal/parse/errors.go
@@ -3,12 +3,13 @@ package parse
 import "errors"
 
 var (
-	ErrEmpty               = errors.New("empty data")
-	ErrNull                = errors.New("null data")
+	ErrEmpty               = errors.New("empty")
+	ErrNull                = errors.New("null")
 	ErrNaN                 = errors.New("NaN")
 	ErrInf                 = errors.New("infinite number")
 	ErrParseInt            = errors.New("could not parse integer")
 	ErrIntOutOfRange       = errors.New("integer out of range")
+	ErrNotString           = errors.New("not a string")
 	ErrParseString         = errors.New("could not parse string")
 	ErrParseTime           = errors.New("could not parse time")
 	ErrParseDuration       = errors.New("could not parse duration")
